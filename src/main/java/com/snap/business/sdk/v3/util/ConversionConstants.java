9880504d95bd5115207a756e092813093f5da176
Wei Luo <wei.luo@snapchat.com>
2024-08-09 13:42:55 -0700

    Add brand field to content item (#3)

--- src/main/java/com/snap/business/sdk/v3/util/ConversionConstants.java
+++ src/main/java/com/snap/business/sdk/v3/util/ConversionConstants.java
@@ -2,8 +2,9 @@ package com.snap.business.sdk.v3.util;
 
 public class ConversionConstants {
     public static final String SDK_LANGUAGE = "java";
-    public static final String SDK_VERSION = "1.1.2";
+    public static final String SDK_VERSION = "1.1.5";
     public static final String USER_AGENT = String.format("BusinessSDK/Java/%s", SDK_VERSION);
+    public static final String USER_AGENT_WITH_PAD = String.format("%s (LaunchPAD)", USER_AGENT);
     public static final String SDK_VER_HEADER = "X-CAPI-V3-BusinessSDK";
     public static final String LANG_VERSION = String.format("%s/%s", SDK_LANGUAGE, SDK_VERSION);
     public static final String INTEGRATION_SDK = "business-sdk-v3";
